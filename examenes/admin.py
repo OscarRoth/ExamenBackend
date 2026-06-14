b3737ee9f669966c76aa906d5c6dee53d89150aa
Oscar Roth <oscarroth@gmail.com>
2026-06-13 21:57:10 -0300

    Admin: registro modelos y creo superusuario

--- examenes/admin.py
+++ examenes/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Alumno,
+    AsignacionExamen,
+    Asignatura,
+    Cursada,
+    Examen,
+    Inscripcion,
+    Pregunta,
+    Profesor,
+    Respuesta,
+)
+
+
+admin.site.register(Profesor)
+admin.site.register(Alumno)
+admin.site.register(Asignatura)
+admin.site.register(Cursada)
+admin.site.register(Inscripcion)
+admin.site.register(Examen)
+admin.site.register(AsignacionExamen)
+admin.site.register(Pregunta)
+admin.site.register(Respuesta)
